6d8a96afd8ecb1075d578694fa7ba74722679e48
Nantha Kishore S <nanthakishore06@gmail.com>
2025-08-13 04:43:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
 const todoList = ['make dinner', 'make dishes'];
-function addTodo(){
-    const inputElem = document.querySelector('.js-input');
-    const name = inputElem.value;
 
-    todoList.push(name);
-    console.log(todoList);
+function addTodo() {
+  const inputElem = document.querySelector('.js-input');
+  const name = inputElem.value;
+  todoList.push(name);
+  inputElem.value = '';
 
-    inputElem.value='';
+  let todoListHTML = '';
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html; // fixed lowercase
+  }
 
-    let todoListHTML = '';
-    for(let i=0;i<todoList.length;i++){
-        const todo = todoList[i];
-        const html = '<p>${todo}</p>';
-        todoListHTML += HTML;
-    }
-    console.log(todoListHTML);
-    document.querySelector('.js-todoList').innerHTML = todoListHTML;
+  document.querySelector('.js-todoList').innerHTML = todoListHTML;
 }
